2f53bd5a85db043ea8f4e74de5c89a3ab807744f
simonmwaura <simonmwaura2090@gmail.com>
2025-06-18 20:46:21 +0300

    i havent done much today and i have to make up for that tomorrow

---
+++ 17.6.2025/8 kyu/remove_the_minimum.py
@@ -0,0 +1,35 @@
+# # The museum of incredibly dull things
+# # The museum of incredibly dull things wants to get rid of some exhibits. Miriam, the interior architect, comes up with a plan to remove the most boring exhibits. She gives them a rating, and then removes the one with the lowest rating.
+
+# # However, just as she finished rating all exhibits, she's off to an important fair, so she asks you to write a program that tells her the ratings of the exhibits after removing the lowest one. Fair enough.
+
+# # Task
+# # Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with the lowest index. If you get an empty array/list, return an empty array/list.
+
+# # Don't change the order of the elements that are left.
+
+# # Examples
+# # * Input: [1,2,3,4,5], output = [2,3,4,5]
+# # * Input: [5,3,2,1,4], output = [5,3,2,4]
+# # * Input: [2,2,1,2,1], output = [2,2,2,1]
+
+# def remove_smallest(numbers):
+#     if len(numbers) == 0: return []
+
+#     new_list = numbers.copy()
+#     minimum = min(numbers)
+
+    
+#     for item in range(len(numbers)):
+#         if numbers[item] != minimum and numbers.count(minimum) == 1:
+#             new_list.append(numbers[item])
+#             min_list = new_list
+            
+#         elif numbers[item] == minimum and numbers.count(minimum) !=1:
+#             min_list = new_list
+#             if numbers[item] :
+#             min_list.remove(numbers[item])
+
+#     return min_list
+
+# print(remove_smallest([1, 2, 3, 4, 1, 5]))
